d1d713fa3810e3169788bb7ba232af17e3e2d023
Arnaudringeval <arnaud.ringeval@yahoo.fr>
2020-11-24 18:17:52 +0100

    majv1

---
+++ seance 2/essai1/js/script.js
@@ -0,0 +1,12 @@
+$('p').animate({
+    width: '50px',
+    height: '20px',
+    fontSize: '35px',
+    marginTop: '50px',
+    color: '#453178',
+    backgroundColor: '#852471'
+}, 1000)
+
+$('p').show(function() {
+    alert("finie")
+})
